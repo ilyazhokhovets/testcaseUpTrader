ee6d50baaee3e883c7b6ecd9f0eb19c8db3517dd
ilyazhokhovets <43029511+ilyazhokhovets@users.noreply.github.com>
2023-04-28 21:42:16 +0300

    3

--- menu/urls.py
+++ menu/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='menu'),
-    re_path(r'^(\w*\/)*$', views.MenuName.as_view())
+
+    re_path(r'^(\w*\/)*$|^$', views.MenuName.as_view())
 
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from .models import *
-from django.db.models import Q
-# Create your views here.
-
-def home(request):
-
-    return render(request, 'menu/main.html')
 
 
 class MenuName(View):
